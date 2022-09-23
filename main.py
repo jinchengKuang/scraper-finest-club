7b3b825e5e52f4b7803e7c3edd65d8dfbab5eb7f
jay <jkuang10@myseneca.ca>
2022-09-22 21:10:30 -0400

    print logs

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
 import time
 from selenium import webdriver
-from selenium.webdriver import ChromeOptions, Chrome
+from selenium.webdriver import ChromeOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-from pynput import keyboard
 from pynput.keyboard import Key, Controller
+from time import strftime
 
 
 def finest_scraper():
@@ -26,44 +26,52 @@ def finest_scraper():
     # input check in date
     driver.execute_script('document.getElementById("id_entrada").removeAttribute("readonly")')
     driver.find_element(by=By.CLASS_NAME, value='engine__date-group').click()
-    time.sleep(2)
+    driver.implicitly_wait(10)
     driver.find_element(by=By.XPATH, value='//a[@data-handler="next"]').click()
-    time.sleep(2)
+    driver.implicitly_wait(10)
     driver.find_element(by=By.XPATH, value='//a[@data-handler="next"]').click()
-    time.sleep(2)
+    driver.implicitly_wait(10)
     driver.find_element(by=By.XPATH, value='//a[@data-handler="next"]').click()
-    time.sleep(2)
+    driver.implicitly_wait(10)
     driver.find_element(by=By.XPATH, value='//td[@data-month="11"]/a[text()="1"]').click()
-    time.sleep(2)
+    driver.implicitly_wait(10)
 
     # input check out date
     driver.execute_script('document.getElementById("id_salida").removeAttribute("readonly")')
     driver.find_element(by=By.XPATH, value='//td[@data-month="11"]/a[text()="2"]').click()
-    time.sleep(2)
+    driver.implicitly_wait(10)
 
     # click check availability button
     driver.find_element(by=By.XPATH, value='//button[@class="engine__action-button"]').click()
-    time.sleep(5)
+    driver.implicitly_wait(10)
 
     # get all headers
     rooms = driver.find_elements(by=By.CSS_SELECTOR, value='.habitacion')
+    print("Number of Room types found: ", len(rooms))
     for room in rooms:
         room_name = room.find_element(by=By.CSS_SELECTOR, value='.hab_titulo').text
+        print(room_name)
         # EXCELLENCE CLUB TWO-STORY ROOFTOP TERRACE SUITE W/ PLUNGE POOL
         if room_name == "EXCELLENCE CLUB TWO-STORY ROOFTOP TERRACE SUITE W/ PLUNGE POOL":
+            print("FOUND")
             # send iMessage
+            print("Sending iMessage...")
             os.system('open -a Messages')
             for i in range(10):
                 time.sleep(3)
                 keyboard = Controller()
                 keyboard.type("GOT IT")
-                time.sleep(2)
+                time.sleep(3)
                 keyboard.press(Key.enter)
 
     driver.quit()
 
 
 if __name__ == '__main__':
+    run_count = 1
     while True:
+        current_time = strftime("%Y-%m-%d %H:%M:%S")
+        print(run_count, current_time)
+        run_count += 1
         finest_scraper()
         time.sleep(1800)
